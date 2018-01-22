d386af6ea1f0169fa1a7fe688dabcba0a8432328
chinaniit <chinaniit@gmail.com>
2018-01-22 17:24:02 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NugetPackageCLIString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = System.IO.File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "source.txt"));
+            var dest = Path.Combine(Directory.GetCurrentDirectory(), "dest.txt");
+
+            List<string> sb = new List<string>();
+            var reg = new Regex(@"  <package id=""(.*?)"" version=""(.*?)"" targetFramework=""net451"" />");
+            foreach(Match match in reg.Matches(source))
+            {
+                var m = "install-package " + match.Groups[1] + " -v " + match.Groups[2];
+                sb.Add(m);
+                Console.WriteLine(m);
+            }
+            if (File.Exists(dest))
+            {
+                File.Delete(dest);
+            }
+            File.AppendAllText(dest, string.Join("\r\n", sb));
+            Console.ReadKey();
+        }
+    }
+}
